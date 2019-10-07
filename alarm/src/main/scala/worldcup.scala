baf746092fb26320df49fe091353665d9da23d2f
Simon Thrane <sthranehansen@gmail.com>
2019-10-06 20:48:25 -0500

    Worked more on translation of world cup

--- alarm/src/main/scala/worldcup.scala
+++ alarm/src/main/scala/worldcup.scala
@@ -60,7 +60,7 @@ import org.sireum._
 
 @record class GroupPhase {
 
-  val gps: Map[GroupName.Type, Set[Score]] = Map.empty ++ ISZ((GroupName.A, CreateSetFromSeq(ISZ(Team.Brazil, Team.Norway, Team.Morocco, Team.Scotland))),
+  var gps: Map[GroupName.Type, Set[Score]] = Map.empty ++ ISZ((GroupName.A, CreateSetFromSeq(ISZ(Team.Brazil, Team.Norway, Team.Morocco, Team.Scotland))),
     (GroupName.B, CreateSetFromSeq(ISZ(Team.Italy, Team.Chile, Team.Austria, Team.Cameroon))),
     (GroupName.C, CreateSetFromSeq(ISZ(Team.France, Team.Denmark, Team.SouthAfrica, Team.SaudiArabia))),
     (GroupName.D, CreateSetFromSeq(ISZ(Team.Nigeria, Team.Paraguay, Team.Spain, Team.Bulgaria))),
@@ -111,9 +111,58 @@ import org.sireum._
 
 
   def Win(wt: Team.Type, wl:Team.Type):Unit={
-    val group = gps.entries.filter(g => g._2.elements.foreach(t => if(t.team.isEqual(wt)) return T)).elements
-    gps = gps -- ISZ(group)
-    gps = gps ++ (group(0)._1)
+    val group = gps.entries.filter(g => g._2.elements.foreach(t => if(t.team.isEqual(wt)) return T)).elements.head
+    gps = gps - group
+    //Update winning team
+    group._2.elements.filter(o => o.team == wt).map(o => {
+      o.points = o.points + 3
+      o.won = o.won + 1
+    })
+    //Update losing team
+    group._2.elements.filter(o => o.team == wt).map(o => {
+      o.lost = o.lost + 1
+    })
+    //Update Groups
+    gps = gps + group
+  }
+
+  def GroupWinner (gp:GroupName.Type):Team.Type= {
+    val scores = gps.get(gp).get
+    var maxPoints : Z = 0
+    scores.elements.foreach(o => if(o.points> maxPoints){maxPoints = o.points})
+    val winners = scores.elements.filter(o => o.points == maxPoints)
+    if(winners.size == 1) {
+      return winners(0).team
+    }
+    else{
+      var maxWins : Z = 0
+      winners.elements.foreach(o => if(o.won > maxWins){maxWins = o.won})
+      winners.elements.filter(o => o.won == maxWins).head.team
+    }
+  }
+
+  def GroupRunnerUp (gp: GroupName.Type):Team.Type ={
+    val groupWinner = GroupWinner(gp)
+    val scores = gps.get(gp).get.elements.filter(tm => tm.team != groupWinner)
+    var maxPoints : Z = 0
+    scores.elements.foreach(o => if(o.points> maxPoints){maxPoints = o.points})
+    val winners = scores.elements.filter(o => o.points == maxPoints)
+    if(winners.size == 1) {
+      return winners(0).team
+    }
+    else{
+      var maxWins : Z = 0
+      winners.foreach(o => if(o.won > maxWins){maxWins = o.won})
+      winners.filter(o => o.won == maxWins).head.team
+    }
+
+  }
+
+
+  def GroupWinners ():Set[Team.Type] = {
+    var winners : Set[Team.Type] = Set.empty
+    gps.entries.foreach(gp => winners + GroupWinner(gp._1))
+    return winners
   }
 
   def CreateSetFromSeq[T](i:ISZ[T]):Set[T]={
