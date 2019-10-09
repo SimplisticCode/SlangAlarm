992c31db8bbe63d6d02e623b620525c2797bf5a6
Simon Thrane <sthranehansen@gmail.com>
2019-10-09 16:56:08 -0500

    Started adding langManual test for Set manipulation

--- alarm/src/test/scala/playground/SetUtilTest.scala
+++ alarm/src/test/scala/playground/SetUtilTest.scala
@@ -8,12 +8,12 @@ import playground.RuntimeUtils.{MapUtil, SetUtil}
 class SetUtilTest extends FunSuite{
 
 
-  val s1 = SetUtil.CreateSetFromSeq(ISZ(Qualification.Chem, Qualification.Bio, Qualification.Mech))
-  val s2 = SetUtil.CreateSetFromSeq(ISZ(Qualification.Elec, Qualification.Chem))
+  val qualificationSet1 = SetUtil.CreateSetFromSeq(ISZ(Qualification.Chem, Qualification.Bio, Qualification.Mech))
+  val qualificationSet2 = SetUtil.CreateSetFromSeq(ISZ(Qualification.Elec, Qualification.Chem))
 
 
   test("Union") {
-    val union = SetUtil.SetUnion(s1, s2)
+    val union = SetUtil.SetUnion(qualificationSet1, qualificationSet2)
     assert(union.size == 4)
     assert(union.contains(Qualification.Chem))
     assert(union.contains(Qualification.Elec))
@@ -22,23 +22,80 @@ class SetUtilTest extends FunSuite{
   }
 
   test("Intersection") {
-    val intersect = SetUtil.SetIntersect(s1, s2)
+    val intersect = SetUtil.SetIntersect(qualificationSet1, qualificationSet2)
     assert(intersect.size == 1)
     assert(intersect.contains(Qualification.Chem))
   }
 
   test("Subset 1") {
-    val isSubset = SetUtil.Subset(s1, s2)
+    val isSubset = SetUtil.Subset(qualificationSet1, qualificationSet2)
     assert(!isSubset)
   }
 
   test("Subset 2") {
     val s1_subSet = SetUtil.CreateSetFromSeq(ISZ(Qualification.Chem, Qualification.Bio, Qualification.Mech))
-    val isSubset = SetUtil.Subset(s1, s1_subSet)
+    val isSubset = SetUtil.Subset(qualificationSet1, s1_subSet)
     assert(isSubset)
-    val isPSubset = SetUtil.PSubset(s1, s1_subSet)
+    val isPSubset = SetUtil.PSubset(qualificationSet1, s1_subSet)
     assert(!isPSubset)
   }
 
 
+  //Test from lang-manual
+  val s1 : Set[Team.Type] = SetUtil.CreateSetFromSeq(ISZ(Team.France, Team.Denmark, Team.SaudiArabia, Team.SouthAfrica))
+  val s2 : Set[Z] = SetUtil.CreateSetFromSeq(ISZ(2,4,6,8,11))
+  val s3 : Set[Z] = Set.empty
+
+  test("Union lang"){
+    val union = SetUtil.SetUnion(s2, s3)
+    assert(union.size == 5)
+    assert(union.contains(2))
+    assert(union.contains(4))
+    assert(union.contains(6))
+    assert(union.contains(8))
+    assert(union.contains(11))
+  }
+
+  test("Intersect lang"){
+    val union = SetUtil.SetIntersect(s2, s3)
+    assert(union.size == 0)
+  }
+
+  test("ProperSubset"){
+    assert(!SetUtil.PSubset(s2, s2))
+  }
+
+  /*
+  test("DUnion"){
+    val vSet1 : Set[Z]= SetUtil.CreateSetFromSeq(ISZ(2,4))
+    val vSet2 : Set[Z] = SetUtil.CreateSetFromSeq(ISZ(4,5,6))
+    val vSet3 : Set[Z]= SetUtil.CreateSetFromSeq(ISZ(0,12))
+
+    val setOfSet : Set[Set[Z]] = SetUtil.CreateSetFromSeq(ISZ(SetUtil.CreateSetFromSeq(ISZ(s2, vSet1, vSet2, vSet3))))
+    val resultSet = SetUtil.SetDUnion(setOfSet)
+
+    assert(resultSet.size == 8)
+    assert(resultSet.contains(0))
+    assert(resultSet.contains(2))
+    assert(resultSet.contains(4))
+    assert(resultSet.contains(5))
+    assert(resultSet.contains(6))
+    assert(resultSet.contains(8))
+    assert(resultSet.contains(11))
+    assert(resultSet.contains(12))
+
+  }
+
+  test("DInter"){
+    val vSet1 : Set[Z]= SetUtil.CreateSetFromSeq(ISZ(2,4))
+    val vSet2 : Set[Z] = SetUtil.CreateSetFromSeq(ISZ(4,5,6))
+
+    val setOfSet : Set[Set[Z]] = SetUtil.CreateSetFromSeq(ISZ(SetUtil.CreateSetFromSeq(ISZ(s2, vSet1, vSet2))))
+    val resultSet = SetUtil.SetDIntersection(setOfSet)
+
+    assert(resultSet.size == 1)
+    assert(resultSet.contains(4))
+
+  }
+*/
 }
